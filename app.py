f241de4002e182039c575ea60aa9729be625592c
Moatasim Farooque <moatasimfarooque@gmail.com>
2025-04-17 04:48:37 +0500

    update

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ db = SQLAlchemy(app)
 if __name__ == '__main__':
     from job_tracker import create_app
     app = create_app()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import argparse
 # Setup command line arguments
 parser = argparse.ArgumentParser(description='Run the Job Tracker application')
 parser.add_argument('--port', type=int, default=5000, help='Port to run the server on')
-parser.add_argument('--host', type=str, default='127.0.0.1', help='Host to run the server on')
+parser.add_argument('--host', type=str, default='0.0.0.0', help='Host to run the server on')
 args = parser.parse_args()
 
 app = create_app()
@@ -15,4 +15,4 @@ with app.app_context():
 
 if __name__ == '__main__':
     print(f"Starting server on {args.host}:{args.port}")
-    app.run(debug=True, host=args.host, port=args.port)
+    app.run(host=args.host, port=args.port)
